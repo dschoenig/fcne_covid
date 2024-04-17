251639548b10b25bf0b6e7d45fec4b5266eded47
Daniel Schoenig <daniel.schoenig@protonmail.com>
2024-04-17 13:32:09 -0400

    GAM clust: reduce RAM

--- src/cc_2_fit_gam_AMZ_6.sh
+++ src/cc_2_fit_gam_AMZ_6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=148G
+#SBATCH --mem=48G
 #SBATCH --time=15:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
--- src/cc_2_fit_gam_AMZ_8.sh
+++ src/cc_2_fit_gam_AMZ_8.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=148G
+#SBATCH --mem=48G
 #SBATCH --time=5:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
