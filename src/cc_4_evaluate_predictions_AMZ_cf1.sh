a65450d95b5820a6236b94d6f8ec8f8aec518cc2
Daniel Schoenig <daniel.schoenig@protonmail.com>
2024-04-24 18:51:38 -0400

    Predictions: cluster

--- src/cc_4_evaluate_predictions_AMZ_cf1.sh
+++ src/cc_4_evaluate_predictions_AMZ_cf1.sh
@@ -2,10 +2,11 @@
 #SBATCH --account=def-cricrime 
 #SBATCH --mem-per-cpu=48G
 #SBATCH --time=5:00:00
+#SBATCH --array=1-10
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
-#SBATCH --job-name=amz_pred_fac
+#SBATCH --job-name=amz_pred_cf1
 
 module load StdEnv/2023 gcc/12.3 gdal/3.7.2 geos/3.12.0 python/3.11.5 udunits/2.2.28 arrow/15.0.1 thrift/0.19.0 r/4.3.1
 
-Rscript 4_evaluate_predictions.R AMZ fac 1 25
+Rscript 4_evaluate_predictions.R AMZ cf1 $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_TASK_COUNT
