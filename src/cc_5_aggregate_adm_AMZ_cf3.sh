e28ba2a96bc94decb78aaba869ffc2bab151d900
Daniel Schoenig <daniel.schoenig@protonmail.com>
2024-04-26 12:16:04 -0400

    Marginal: aggregation by factual and counterfactual

--- src/cc_5_aggregate_adm_AMZ_cf3.sh
+++ src/cc_5_aggregate_adm_AMZ_cf3.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=128G
-#SBATCH --time=24:00:00
+#SBATCH --mem=100G
+#SBATCH --time=3:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
-#SBATCH --job-name=amz_mar_adm
+#SBATCH --job-name=amz_adm_cf3
 
 module load StdEnv/2023 gcc/12.3 gdal/3.7.2 geos/3.12.0 python/3.11.5 udunits/2.2.28 arrow/15.0.1 thrift/0.19.0 r/4.3.1
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-Rscript 6_marginal_adm.R 4 amz
+Rscript 5_aggregate_adm.R 4 amz cf3
