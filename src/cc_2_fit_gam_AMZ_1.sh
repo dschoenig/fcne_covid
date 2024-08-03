b040d906235927883245f3382caf8cc2c1752483
Daniel Schoenig <daniel.schoenig@protonmail.com>
2024-08-03 12:09:15 -0400

    Resource allocation

--- src/cc_2_fit_gam_AMZ_1.sh
+++ src/cc_2_fit_gam_AMZ_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=50G
+#SBATCH --mem=64G
 #SBATCH --time=10:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
