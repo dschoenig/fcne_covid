b7cefbc60b336be56c1429b0f4256082d9e1fb3e
Daniel Schoenig <daniel.schoenig@protonmail.com>
2024-08-06 16:18:58 -0400

    Marginal: fix arguments cluster scripts

--- src/cc_5_marginal_covid_mort_AMZ.sh
+++ src/cc_5_marginal_covid_mort_AMZ.sh
@@ -11,4 +11,4 @@ module load StdEnv/2023 gcc/12.3 gdal/3.7.2 geos/3.12.0 python/3.11.5 udunits/2.
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-Rscript 5_marginal_covid.R 4 amz covid.mortlag1
+Rscript 5_marginal.R 4 amz covid.mort
