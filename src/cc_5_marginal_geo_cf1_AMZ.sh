5cc9fe1acbe8777a3adcb250d2c5e25e7f1f22a0
Daniel Schoenig <schoed@narval3.narval.calcul.quebec>
2024-08-09 16:59:51 -0400

    Marginals: resource allocation

--- src/cc_5_marginal_geo_cf1_AMZ.sh
+++ src/cc_5_marginal_geo_cf1_AMZ.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=96G
-#SBATCH --time=16:00:00
+#SBATCH --time=32:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 #SBATCH --job-name=amz_mar_geo_cf1
--- src/cc_5_marginal_ten_fac_AMZ.sh
+++ src/cc_5_marginal_ten_fac_AMZ.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=96G
-#SBATCH --time=12:00:00
+#SBATCH --time=18:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 #SBATCH --job-name=amz_mar_ten_fac
