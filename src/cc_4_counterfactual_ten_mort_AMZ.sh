743f66b6edc995c721799e172a4bc97bd5e08946
Daniel Schoenig <daniel.schoenig@protonmail.com>
2024-08-27 15:36:11 -0400

    Fix typo

--- src/cc_4_counterfactual_ten_mort_AMZ.sh
+++ src/cc_4_counterfactual_ten_mort_AMZ.sh
@@ -11,5 +11,5 @@ module load StdEnv/2023 gcc/12.3 gdal/3.7.2 geos/3.12.0 python/3.11.5 udunits/2.
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-Rscript 4_counterfactual_ten.R 4 amz mort itpa
-Rscript 4_counterfactual_ten.R 4 amz mortlag1 itpa
+Rscript 4_counterfactual_ten_mort.R 4 amz mort itpa
+Rscript 4_counterfactual_ten_mort.R 4 amz mortlag1 itpa
