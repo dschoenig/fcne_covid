3963efa595e734e27d218c513eba9c626304804d
Daniel Schoenig <daniel.schoenig@protonmail.com>
2024-08-28 10:35:55 -0400

    Resource allocation

--- src/cc_4_counterfactual_geo_AMZ.sh
+++ src/cc_4_counterfactual_geo_AMZ.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=48G
-#SBATCH --time=5:00:00
+#SBATCH --mem=96G
+#SBATCH --time=3:00:00
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
 #SBATCH --job-name=cf_geo_amz
--- src/cc_4_evaluate_predictions_AMZ_deg_cf1.sh
+++ src/cc_4_evaluate_predictions_AMZ_deg_cf1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-cricrime 
 #SBATCH --mem-per-cpu=48G
-#SBATCH --time=5:00:00
+#SBATCH --time=8:00:00
 #SBATCH --array=1-50
 #SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
 #SBATCH --mail-type=ALL
