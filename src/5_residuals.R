54bc5fb0edb13bcbca2453f7a5212008ec031fc6
Daniel Schoenig <daniel.schoenig@protonmail.com>
2024-08-29 15:58:51 -0400

    Fix typo

--- src/5_residuals.R
+++ src/5_residuals.R
@@ -14,7 +14,7 @@ path.base <- "../"
 path.data.proc <- paste0(path.base, "data/processed/")
 path.mod <- paste0(path.base, "models/gam/")
 path.pred <- paste0(path.mod, "pred/")
-path.arrow <- paste0(path.pred, region, "/", resp_type, "/fac/",)
+path.arrow <- paste0(path.pred, region, "/", resp_type, "/fac/")
 file.data <- paste0(path.data.proc, region, ".data.fit.proc.rds")
 files.pred <- list.files(path.arrow, ".arrow", full.names = TRUE)
 
