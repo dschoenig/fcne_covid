fb457212ec8b3301d413339043c49225bb9b1eb3
Daniel Schoenig <daniel.schoenig@protonmail.com>
2024-08-30 07:24:10 -0400

    Test posterior sim

---
+++ src/3_posterior_simulation_test.R
@@ -0,0 +1,44 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+library(mgcv)
+library(mvnfast)
+library(posterior)
+
+source("utilities.R")
+
+region <- tolower(as.character(args[1]))
+model.resp <- tolower(as.character(args[2]))
+model.id <- as.integer(args[3])
+n.threads <- ifelse(length(args) < 3, 1, as.integer(args[4]))
+# region <- "cam"
+# n.threads <- 4
+
+path.gam <- "../models/gam/"
+
+seed <- 18980605
+
+
+## SIMULATION FROM MULTIVARIATE NORMAL APPROXIMATION OF MODEL POSTERIOR ########
+
+file.model <- paste0(path.gam, region, ".test.m.", model.id, ".", model.resp, ".rds")
+file.post <- paste0(path.gam, region, ".test.m.",  model.id, ".", model.resp, ".post.rds")
+
+paste0("Simulating 1000 draws from the approximate posterior distribution\n",
+       "of model ", file.model, " …") |>
+message()
+
+# Load model
+model <- readRDS(file.model)
+
+
+# Posterior draws
+set.seed(seed)
+post <- 
+  egp_posterior_draw(model,
+                     n = 1000,
+                     unconditional = TRUE,
+                     package = "mvnfast",
+                     parallel = 4)
+
+saveRDS(post, file.post)
+
---
+++ src/cc_3_posterior_simulation_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=def-cricrime 
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=36G
+#SBATCH --time=01:00:00
+#SBATCH --mail-user=schonig.daniel@courrier.uqam.ca
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=AMZ_post_1
+
+module load StdEnv/2023 gcc/12.3 gdal/3.7.2 geos/3.12.0 python/3.11.5 udunits/2.2.28 arrow/15.0.1 thrift/0.19.0 r/4.3.1
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+Rscript 3_posterior_simulation_test.R AMZ dis 1 $SLURM_CPUS_PER_TASK
+Rscript 3_posterior_simulation_test.R AMZ dis 2 $SLURM_CPUS_PER_TASK
+Rscript 3_posterior_simulation_test.R AMZ dis 3 $SLURM_CPUS_PER_TASK
+Rscript 3_posterior_simulation_test.R AMZ dis 4 $SLURM_CPUS_PER_TASK
+Rscript 3_posterior_simulation_test.R AMZ dis 5 $SLURM_CPUS_PER_TASK
+
